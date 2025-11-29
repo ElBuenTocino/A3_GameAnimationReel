ae3c2cdc2efee8ae3e77cdbc2d8a064fdf4bb651
serar3 <sergio.alsa33@gmail.com>
2025-11-29 16:37:25 +0100

    acabao?

--- A3_AnimationReel/Assets/Art/ArmAndo/armandoParticles.cs
+++ A3_AnimationReel/Assets/Art/ArmAndo/armandoParticles.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class armandoParticles : MonoBehaviour
 {
     public ParticleSystem aterriza;
+    public ParticleSystem golpeVoley;
     public void ParticlesArmando (int event_)
     {
         Debug.Log("Event: " + event_);
-        if(event_ == 0)
+        if(event_ == 1)
         {
             aterriza.Play();
+        }else if (event_ == 2)
+        {
+            golpeVoley.Play();
         }
     }
 }
